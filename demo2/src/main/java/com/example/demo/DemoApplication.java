999782647da9553b0b42cf1cb8872bc4de1a438d
abasaric <basaric.aleksandar@gmail.com>
2018-10-16 17:46:39 +0200

    initial commit

---
+++ demo2/src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		//SpringApplication.run(DemoApplication.class, args);
+		
+		System.out.println("Hello world");
+		System.out.println("Hello world");
+		System.out.println("Hello world");
+		System.out.println("Hello world");
+		System.out.println("Hello world");
+		System.out.println("promena nad demo samo za aleka yeayeyaeyayea");
+	}
+}
+
